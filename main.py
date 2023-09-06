cad8f3c4545ae990e53fc7ffa1b10663e33f6f9e
Kritsana <poddknws@gmail.com>
2023-09-06 08:24:17 +0700

    Connect to database

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import requests
 import selectorlib
 import time
+import sqlite3
 from send_email import send_email
 
-
-"INSERT INTO events VALUES('Tiger', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.14'"
-"DELETE FROM events WHERE band='Tiger'"
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish a connection and cursor
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
 
 
 def scrape(url):
@@ -28,26 +29,33 @@ def extract(source):
 
 
 def store(extracted):
-    # with open("data.txt", "w") as file:
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    # cursor.execute("SELECT * FROM events WHERE date='2088.10.14'")
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    # print(rows)
+    return rows
 
 
 if __name__ == "__main__":
-    # print(scrape(URL))
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         # print(extracted)
-        # store(extracted)
-        content = read(extracted)
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new event was found!")
         time.sleep(2)
